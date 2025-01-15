bc04dd7999603a894708690d204107e368fa0928
Michael Smyer <michael.smyer@cybernowinstitute.com>
2025-01-15 11:32:27 -0600

    Adds videojs global config

--- script.js
+++ script.js
@@ -15,6 +15,37 @@
   const v = d.createElement("script");
   v.src = "https://vjs.zencdn.net/8.16.1/video.min.js";
   v.async = true;
+  v.onload = function () {
+    videojs.options.controlBar = {
+      children: [
+        "playToggle",
+        "volumePanel",
+        "progressControl",
+        "remainingTimeDisplay",
+        "playbackRateMenuButton",
+        "subsCapsButton",
+        "qualitySelector",
+        "fullscreenToggle",
+      ],
+    };
+    videojs.options.fill = true;
+    videojs.options.textTrackSettings = false;
+    videojs.options.controls = true;
+
+    videojs.options.html5.vhs = {
+      overrideNative: true,
+      handleManifestRedirects: true,
+      withCredentials: true,
+    };
+
+    videojs.options.html5.nativeAudioTracks = false;
+    videojs.options.html5.nativeTextTracks = videojs.browser.IS_IPHONE;
+    videojs.options.html5.nativeVideoTracks = false;
+
+    videojs.options.playbackRates = [1, 1.25, 1.5, 1.75, 2];
+    videojs.options.preferFullWindow = true;
+    videojs.options.responsive = true;
+  };
   d.head.appendChild(v);
   const l = d.createElement("link");
   l.rel = "stylesheet";
