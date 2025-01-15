738245355a27b4e00acd299075fceb43d8d8c73d
Michael Smyer <michael.smyer@cybernowinstitute.com>
2025-01-15 10:16:28 -0600

    Adds video.js loading code
    Note: the code has been formatted by Prettier

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Google Tag Manager
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WZWX4NXZ');
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : "";
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", "GTM-WZWX4NXZ");
+
+// Video.js for HLS support
+(function (d) {
+  const v = d.createElement("script");
+  v.src = "https://vjs.zencdn.net/8.16.1/video.min.js";
+  v.async = true;
+  d.head.appendChild(v);
+  const l = d.createElement("link");
+  l.rel = "stylesheet";
+  l.href = "https://vjs.zencdn.net/8.16.1/video-js.css";
+  v.async = true;
+  d.head.appendChild(l);
+})(document);
